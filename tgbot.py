f7219f730aec1048fca862e0ba7b61073a357681
kaminaenzo1 <interesningobuna@gmail.com>
2024-12-09 09:55:48 +0500

    Update tgbot.py
    version 2

--- tgbot.py
+++ tgbot.py
@@ -1,4 +1,4 @@
-mport asyncio
+import asyncio
 from aiogram import Bot,Dispatcher,types
 from aiogram.filters import Command
 
@@ -17,3 +17,14 @@ async def start(message:types.Message):
     await message.answer(f"Assalomu alaykum! Iltimos ismingizni kiriting:")
     await ask_phone(message)
     print(user_data)
+async def ask_phone(message:types.Message):
+    user_id = message.from_user.id
+    name = message.text
+    user_data[user_id]['name'] = name
+    button = [
+        [types.KeyboardButton(text='Share Contact',request_contact=True)]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=button,one_time_keyboard=True,resize_keyboard=True)
+    await message.answer(f"Iltimos telefon raqamingizni jo'nating",reply_markup=keyboard)
+    print(user_data)
+
